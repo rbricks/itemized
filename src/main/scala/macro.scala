ba77aa9b84178279d97e4e6898805c57958f81b3
Andrea Lattuada <utaaal@gmail.com>
2016-02-04 19:06:32 +0100

    macro to generate IndexedCaseEnum-compliant ADT

--- src/main/scala/macro.scala
+++ src/main/scala/macro.scala
@@ -23,7 +23,7 @@ import scala.annotation.compileTimeOnly
  * is transformed to
  *
  * ```
- * sealed abstract trait Planet
+ * sealed abstract trait Planet extends CaseEnum
  * object Planet {
  *   case object Earth extends Planet
  *   case object Venus extends Planet
@@ -63,7 +63,81 @@ object EnumMacro {
     }
 
     annottees.map(_.tree) match {
-      //case (classDecl: ClassDef) :: Nil => modifiedClass(classDecl, None)
+      case (classDecl: ClassDef) :: Nil => modifiedClass(classDecl)
+      case _ => c.abort(c.enclosingPosition, "Invalid annottee")
+    }
+  }
+}
+
+/**
+ * Macro annotation that transforms a trait enclosing a single type
+ * alias and a set of objects to an ADT following the IndexedCaseEnum
+ * convention (see the IndexedCaseEnum trait).
+ *
+ * e.g.
+ * ```
+ * @indexedEnum trait Planet {
+ *   type Index = Int
+ *   object Earth   { 1 }
+ *   object Venus   { 2 }
+ *   object Mercury { 3 }
+ * }
+ * ```
+ *
+ * is transformed to
+ *
+ * ```
+ * sealed abstract trait Planet extends IndexedEnum {
+ *   type Index = Int
+ * }
+ * object Planet {
+ *   case object Earth extends Planet { val index = 1 }
+ *   case object Venus extends Planet { val index = 2 }
+ *   case object Mercury extends Planet { val index = 3 }
+ * }
+ * ```
+ */
+@compileTimeOnly("Enable macro paradise to expand macro annotations.")
+class indexedEnum extends StaticAnnotation {
+  def macroTransform(annottees: Any*) = macro IndexedEnumMacro.impl
+}
+ 
+object IndexedEnumMacro {
+  def impl(c: Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
+    import c.universe._
+ 
+    def modifiedClass(classDecl: ClassDef) = {
+      val (enumName, body) = try {
+        val q"trait $enumName { ..$body }" = classDecl
+        (enumName, body)
+      } catch {
+        case _: MatchError =>
+          c.abort(c.enclosingPosition, "Annotation is only supported on objects")
+      }
+      val typeAliasTree :: memberTrees = body
+      val members = memberTrees.map {
+        case q"object $memberName { $statement }" =>
+          q"""case object $memberName extends $enumName {
+                val index = $statement
+              }"""
+        case _ =>
+          c.abort(c.enclosingPosition, "Enum members should be plain objects")
+      }
+      val indexType = typeAliasTree match {
+        case q"type Index = $ttype" => ttype
+        case _ => c.abort(c.enclosingPosition, "Invalid type alias declaration")
+      }
+      c.Expr(q"""
+        sealed abstract trait $enumName extends _root_.ingredients.caseenum.IndexedCaseEnum {
+          type Index = $indexType
+        }
+        object ${enumName.toTermName} {
+          ..$members
+        }
+      """)
+    }
+
+    annottees.map(_.tree) match {
       case (classDecl: ClassDef) :: Nil => modifiedClass(classDecl)
       case _ => c.abort(c.enclosingPosition, "Invalid annottee")
     }
--- src/test/scala/CaseEnumMacroSpec.scala
+++ src/test/scala/CaseEnumMacroSpec.scala
@@ -1,4 +1,4 @@
-import ingredients.caseenum.annotations.enum
+import ingredients.caseenum.annotations.{enum, indexedEnum}
 
 import org.scalatest.{ Matchers, WordSpec }
 
@@ -16,6 +16,28 @@ class CaseEnumMacroSpec extends WordSpec with Matchers {
       Planet.Earth shouldBe a[Planet]
       Planet.Mercury should not be a [Planet.Earth.type]
       Planet.Earth shouldBe Planet.Earth
+    }
+  }
+
+}
+
+class IndexedCaseEnumMacroSpec extends WordSpec with Matchers {
+  @indexedEnum trait Planet {
+    type Index = Int
+    object Mercury { 1 }
+    object Venus   { 2 }
+    object Earth   { 3 }
+  }
+
+  "@indexedEnum annotation" should {
+    "produce a valid IndexedCaseEnum-style ADT" in {
+      val typecheck: Int = 3: Planet#Index
+      Planet.Earth shouldBe a[Product]
+      Planet.Earth shouldBe a[Serializable]
+      Planet.Earth shouldBe a[Planet]
+      Planet.Mercury should not be a [Planet.Earth.type]
+      Planet.Earth shouldBe Planet.Earth
+      Planet.Earth.index shouldBe 3
     }
   }
 
